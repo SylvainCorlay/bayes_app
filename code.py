79ae105e1cfbd25a4ca89efdf80a9019c09e3ccf
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-04-23 16:25:27 -0400

    ...

--- code.py
+++ code.py
@@ -1,15 +1,7 @@
 from IPython.display import display
 from ipywidgets import jslink,  IntSlider
-from traitlets import link
 
-from ipyleaflet import (
-    Map,
-    Marker,
-    TileLayer, ImageOverlay,
-    Polyline, Polygon, Rectangle, Circle, CircleMarker,
-    GeoJSON,
-    DrawControl
-)
+from ipyleaflet import Map
 
 m = Map(center=[34.6252978589571, -77.34580993652344])
 zoom_slider = IntSlider(description='Zoom', min=3, max=17, value=10)
